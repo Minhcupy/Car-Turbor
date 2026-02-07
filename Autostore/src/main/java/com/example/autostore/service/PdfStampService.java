1666ad72d3d24f40f476cd545cdb24ec4ac23b4b
MinhCupy <tqmminh2004tqm@gmail.com>
2026-02-07 15:22:43 +0700

    upadte

--- Autostore/src/main/java/com/example/autostore/service/PdfStampService.java
+++ Autostore/src/main/java/com/example/autostore/service/PdfStampService.java
@@ -2,21 +2,27 @@ package com.example.autostore.service;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 @Service
 public class PdfStampService {
 
     public byte[] stampSignature(byte[] pdfBytes, byte[] pngBytes, String signerName) {
         try (PDDocument doc = PDDocument.load(pdfBytes);
-             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+             ByteArrayOutputStream out = new ByteArrayOutputStream();
+             InputStream fontIs = new ClassPathResource("fonts/DejaVuSans.ttf").getInputStream()) {
 
             var page = doc.getPage(doc.getNumberOfPages() - 1);
             var img = PDImageXObject.createFromByteArray(doc, pngBytes, "signature");
+
+            // Unicode font (embed = true)
+            PDType0Font font = PDType0Font.load(doc, fontIs, true);
 
             float imgW = 170, imgH = 65;
             var box = page.getMediaBox();
@@ -29,7 +35,7 @@ public class PdfStampService {
                 cs.drawImage(img, x, y, imgW, imgH);
 
                 cs.beginText();
-                cs.setFont(PDType1Font.HELVETICA, 10);
+                cs.setFont(font, 10);
                 cs.newLineAtOffset(x, y - 12);
                 cs.showText("Signed by: " + signerName);
                 cs.endText();
